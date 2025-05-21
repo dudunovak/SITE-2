e0b7443b82e4c6c9dceb408f7f2439ded23fc1d9
Eduardo Novak <140639419+dudunovak@users.noreply.github.com>
2025-05-21 14:24:26 -0300

    89765

---
+++ main.js
@@ -0,0 +1,14 @@
+const botoes = document.querySelectorAll(".botoes");
+const textos = document.querySelectorAll(".aba-conteudo");
+for (let i=0; i <botoes.length; i++){
+    botoes [i] .onclick = function(){
+        for (let j=0; j < botoes.length; jj++){
+            botoes [j] .classList.remove ("ativo");
+            textos [j] .classList.remove ("ativo");
+        }
+        botoes [i] .classList.add ("ativo");
+        textos [i] .classList.add ("ativo");
+    }
+}
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date ("2025-10-10T09:34:21")

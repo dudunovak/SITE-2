475871da79f71e4f93691aadb43913156245286e
Eduardo Novak <140639419+dudunovak@users.noreply.github.com>
2025-06-04 14:16:12 -0300

    23456

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ comecaCronometro();
 function calculaTempo (tempoObjetivo){
     let tempoAtual = new Date ();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = match.floor(tempoFinal/1000);
+    let segundos = Match.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);

e89cc0eb345038601f6b4254f87876a740b8e87e
Eduardo Novak <140639419+dudunovak@users.noreply.github.com>
2025-06-11 07:49:40 -0300

    234

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ for (let i=0; i <botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2027-08-10T09:00:00");
-const tempoObjetivo2 = new Date ("2030-09-20T19:00:00");
-const tempoObjetivo3 = new Date ("2026-02-29T00:00:00");
-const tempoObjetivo4 = new Date ("2025-09-01T00:00:00");
+const tempoObjetivo1 = new Date ("2027-08-10T09:01:00");
+const tempoObjetivo2 = new Date ("2030-09-20T19:20:00");
+const tempoObjetivo3 = new Date ("2026-02-29T17:04:00");
+const tempoObjetivo4 = new Date ("2025-09-01T13:20:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function atualizaCronometro (){
     for (let i = 0; i <contadores.length; i++){
@@ -49,4 +49,5 @@ function calculaTempo (tempoObjetivo){
     }else{
         return [0,0,0,0]
     }
+    
 }
